f4a3e73a2f04c1b50f2d893102f04d80a178a46b
Andy Fraley <andy_fraley@hotmail.com>
2016-01-20 21:19:46 -0500

    get_pledges basically working except for ability to specify output CSV file from command line

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ccb_backup
 Backup utilities for Church Community Builder (CCB)
+
+These are under development (non-working state) for now.  2016-01-20
--- get_pledges.py
+++ get_pledges.py
@@ -2,13 +2,7 @@
 
 # TODOs
 #
-# See big TODO at bottom of file
-#
-# Check this into git "backup" repository.  Make it work for CCB in general.  Release it??
-#
 # Add command-line args for output file
-#
-# Source password info outside of file (see old API utils for how to)
 #
 # Wrapper Python script to call get_pledges.py, get_individuals.py, etc.  Then take all results and ZIP them up
 # into posted backup file into S3
@@ -27,6 +21,7 @@ from util import settings
 def main(argv):
 
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.getLogger().setLevel(logging.INFO)
 
     login_request = {
         'ax': 'login',
@@ -54,15 +49,35 @@ def main(argv):
         'output': 'export'
     }
 
-    pledge_detail_report_info = {
+    pledge_detail_dialog_report_info = {
         "type":"pledge_giving_detail",
         "id":""
     }
 
-    pledge_detail_request = {
+    pledge_detail_dialog_request = {
         'aj': 1,
         'ax': 'create_modal',
-        'request': json.dumps(pledge_detail_report_info)
+        'request': json.dumps(pledge_detail_dialog_report_info),
+    }
+
+    pledge_detail_report_info = {
+        'id':'',
+        'type': 'pledge_giving_detail',
+        'transaction_detail_type_id': '{coa_id}', # {coa_id} is substituted at run-time
+        'print_type': 'family',
+        'split_child_records': '1',
+        'show': 'all',
+        'date_range': '',
+        'ignore_static_range': 'static',
+        'start_date': '01/01/2000',
+        'end_date': curr_date_str,
+        'campus_ids': ['1'],
+        'output': 'csv'
+    }
+
+    pledge_detail_request = {
+        'request': json.dumps(pledge_detail_report_info), # This is also replaced at run-time
+        'output': 'export'
     }
 
     with requests.Session() as http_session:
@@ -102,13 +117,12 @@ def main(argv):
 
         # Get dictionary of category option IDs
         report_page = http_session.get('https://ingomar.ccbchurch.com/service/report_settings.php',
-            params=pledge_detail_request)
+            params=pledge_detail_dialog_request)
         if report_page.status_code == 200:
             match_report_options = re.search(
                 '<select\s+name=\\\\"transaction_detail_type_id\\\\"\s+id=\\\\"\\\\"\s*>(.*?)<\\\/select>',
                 report_page.text)
             pledge_categories_str = match_report_options.group(1)
-        
         else:
             logging.error('Error retrieving report settings page. Aborting!')
             sys.exit(1)
@@ -123,11 +137,39 @@ def main(argv):
                 root_str = option_match.group(2)
                 dict_pledge_categories[root_str] = int(option_match.group(1))
 
-        for pledge_category in list_pledge_categories:
-            if pledge_category in dict_pledge_categories:
-                print pledge_category, dict_pledge_categories[pledge_category]
-            else:
-                logging.warning('Unknown pledge category. ' + pledge_category)
+        output_csv_header = None
+        with open('./output_file.csv', 'wb') as csv_output_file:
+            csv_writer = csv.writer(csv_output_file)
+            for pledge_category in list_pledge_categories:
+                logging.info('Retrieving pledges for ' + pledge_category)
+                if pledge_category in dict_pledge_categories:
+                    pledge_detail_report_info['transaction_detail_type_id'] = \
+                        str(dict_pledge_categories[pledge_category])
+                    pledge_detail_request['request'] = json.dumps(pledge_detail_report_info)
+                    pledge_detail_response = http_session.post('https://ingomar.ccbchurch.com/report.php',
+                        data=pledge_detail_request)
+                    pledge_detail_succeeded = False
+                    if pledge_detail_response.status_code == 200:
+                        pledge_detail_succeeded = True
+                        csv_reader = csv.reader(StringIO.StringIO(pledge_detail_response.text.encode('ascii',
+                            'ignore')))
+                        header_row = True
+                        for row in csv_reader:
+                            if header_row:
+                                if output_csv_header is None:
+                                    output_csv_header = ['COA ID', 'COA Category'] + row
+                                    amount_column_index = output_csv_header.index('Total Pledged')
+                                    print 'Amount column index is ' + str(amount_column_index)
+                                    csv_writer.writerow(output_csv_header)
+                                header_row = False
+                            else:
+                                row = [dict_pledge_categories[pledge_category], pledge_category] + row
+                                if row[amount_column_index] != '0':
+                                    csv_writer.writerow(row)
+                    if not pledge_detail_succeeded:
+                        logging.warning('Pledge Detail retrieval failure for category ' + pledge_category)
+                else:
+                    logging.warning('Unknown pledge category. ' + pledge_category)
 
 
 if __name__ == "__main__":
