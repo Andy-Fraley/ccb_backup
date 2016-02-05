d4524cce90247bed5545579130954d50ebc94275
Andy Fraley <andy_fraley@hotmail.com>
2016-02-05 16:54:40 -0500

    All utilities are run by ccb_backup.py.  Now need to ZIP results and post to AWS S3

--- ccb_backup.py
+++ ccb_backup.py
@@ -14,6 +14,8 @@ from util import util
 class g:
     args = None
     program_filename = None
+    temp_directory = None
+    message_output_filename = None
 
 
 def main(argv):
@@ -33,42 +35,53 @@ def main(argv):
 
     message_level = util.get_ini_setting('logging', 'level')
 
-    temp_directory = tempfile.mkdtemp(prefix='ccb_backup_')
+    g.temp_directory = tempfile.mkdtemp(prefix='ccb_backup_')
 
     if g.args.message_output_filename is None:
-        message_output_filename = temp_directory + '/messages_' + \
+        g.message_output_filename = g.temp_directory + '/messages_' + \
             datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.log'
     else:
-        message_output_filename = g.args.message_output_filename
+        g.message_output_filename = g.args.message_output_filename
 
-    util.set_logger(message_level, message_output_filename, os.path.basename(__file__))
+    util.set_logger(message_level, g.message_output_filename, os.path.basename(__file__))
 
     if g.args.output_filename is not None:
         output_filename = g.args.output_filename
     else:
         output_filename = './tmp/ccb_backup_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.zip'
     
-    # Gather pledges
-    pledges_filename = temp_directory + '/pledges_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
-    message_info('Running get_pledges.py with output file ' + pledges_filename)
-    program_filename = 'get_pledges.py'
-    pledges_py = os.path.dirname(os.path.realpath(__file__)) + '/' + program_filename
-    shell_result = subprocess.call([pledges_py, '--output-filename', pledges_filename, '--message-output-filename',
-        message_output_filename])
-    abort_if_non_zero(shell_result, program_filename, temp_directory)
-
-    # Gather attendance and events
-    attendance_filename = temp_directory + '/attendance_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
-    events_filename = temp_directory + '/events_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
-    message_info('Running get_attendance.py with output files ' + attendance_filename + ' and ' + events_filename)
-    program_filename = 'get_attendance.py'
-    attendance_py = os.path.dirname(os.path.realpath(__file__)) + '/' + program_filename
-    shell_result = subprocess.call([attendance_py, '--output-events-filename', events_filename,
-        '--output-attendance-filename', attendance_filename, '--message-output-filename', message_output_filename])
-    abort_if_non_zero(shell_result, program_filename, temp_directory)
+    # Run get_XXX.py utilities into datetime_stamped CSV output files and messages_output.log output in temp directory
+    run_util('individuals')
+    run_util('groups', ['groups', 'participants'])
+    run_util('attendance', ['attendance', 'events'])
+    run_util('pledges')
+    run_util('contributions')
 
     #shutil.rmtree(temp_directory)
     sys.exit(0)
+
+
+def run_util(util_name, output_pair=None):
+    global g
+
+    datetime_stamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    if output_pair is not None:
+        output_filename1 = g.temp_directory + '/' + output_pair[0] + '_' + datetime_stamp + '.csv'
+        output_filename2 = g.temp_directory + '/' + output_pair[1] + '_' + datetime_stamp + '.csv'
+        outputs_list = ['--output-' + output_pair[0] + '-filename', output_filename1,
+            '--output-' + output_pair[1] + '-filename', output_filename2]
+        message_info('Running get_' + util_name + '.py with output files ' + output_filename1 + ' and ' + \
+            output_filename2)
+    else:
+        output_filename = g.temp_directory + '/' + util_name + '_' + datetime_stamp + '.csv'
+        outputs_list = ['--output-filename', output_filename]
+        message_info('Running get_' + util_name + '.py with output file ' + output_filename)
+    util_py = 'get_' + util_name + '.py'
+    fullpath_util_py = os.path.dirname(os.path.realpath(__file__)) + '/' + util_py
+    exec_list = [fullpath_util_py, '--message-output-filename', g.message_output_filename] + outputs_list
+    print exec_list
+    shell_result = subprocess.call(exec_list)
+    abort_if_non_zero(shell_result, util_py, g.temp_directory)
 
 
 def abort_if_non_zero(shell_result, program_filename, temp_directory):
